2b59dc45d0caec5ac25c0d089509aacba3a57a02
Damian Makki <damian@makki.pro>
2018-04-25 14:51:43 -0700

    Initial commit of Sourced Cheap Flights landing page.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sourced Cheap Flights
+
+The cheapest flights to the places you actually want to go. From your friends at [Sourced Adventures](https://www.sourcedadventures.com).
---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp');  
+var sass = require('gulp-sass');  
+
+gulp.task('sass', function () {  
+    gulp.src('sass/main.scss')
+        .pipe(sass({includePaths: ['scss']}))
+        .pipe(gulp.dest('css'));
+});
+
+gulp.task('default', ['sass',], function () {  
+    gulp.watch("sass/**/*.scss", ['sass']);
+});
