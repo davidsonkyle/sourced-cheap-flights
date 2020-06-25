b45baf6f54f84b096ca91ce85eaf34c5845547ab
Damian Makki <damian@makki.pro>
2020-06-25 16:44:08 -0700

    Clean up gulpfile, update package.json, and remove yarn.lock.

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,70 @@
-var gulp = require('gulp');  
-var sass = require('gulp-sass');  
+var gulp = require("gulp"),
+    plumber = require('gulp-plumber'),
+    rename = require('gulp-rename'),
+    sass = require("gulp-sass"),
+    concat = require('gulp-concat'),
+    postcss = require("gulp-postcss"),
+    autoprefixer = require("autoprefixer"),
+    cssnano = require("cssnano"),
+    notify = require('gulp-notify'),
+    gutil = require('gulp-util'),
+    addsrc = require('gulp-add-src');
 
-gulp.task('sass', function () {  
-    gulp.src('sass/main.scss')
-        .pipe(sass({includePaths: ['scss']}))
-        .pipe(gulp.dest('css'));
-});
+var paths = {
+    styles: {
+        src: "sass/**/*.scss",
+        dest: "dist/css/"
+    }
+};
 
-gulp.task('default', ['sass',], function () {  
-    gulp.watch("sass/**/*.scss", ['sass']);
-});
+function style() {
+    return gulp
+        .src(['sass/main.scss'])
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(sass())
+        .on('error', function (err) {
+            notify().write(err);
+            this.emit('end');
+        })
+        .pipe(rename({
+            basename: "main",
+        }))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(gulp.dest(paths.styles.dest))
+        .pipe(notify({ message: 'Styles compiled successfully.' }));
+}
+
+function script() {
+
+    return gulp.src('js/main.js')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.log(error.message);
+                this.emit('end');
+            }
+        }))
+        .pipe(concat('main.js'))
+        .pipe(rename({ suffix: '.min' }))
+        .on('error', function (err) { gutil.log(gutil.colors.red('[Error]'), err.toString()); })
+        .pipe(gulp.dest('dist/js/'))
+        .pipe(notify({ message: 'JS compiled successfully.' }));
+}
+
+function watch() {
+    gulp.watch(paths.styles.src, style);
+    gulp.watch("js/**/*.js", script);
+
+}
+
+exports.watch = watch;
+exports.style = style;
+exports.script = script;
+
+var build = gulp.parallel(style, script, watch);
+
+gulp.task('default', build);
